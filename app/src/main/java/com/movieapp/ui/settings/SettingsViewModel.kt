61166a7940237b4c2d3e62f6056be170dc8b3623
Faisal Tai <taifaisal57@gmail.com>
2021-10-06 12:54:50 +0530

    Added Fragments

--- app/src/main/java/com/movieapp/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/movieapp/ui/settings/SettingsViewModel.kt
@@ -1,13 +1,13 @@
-package com.movieapp.ui.dashboard
+package com.movieapp.ui.settings
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Settings Fragment"
     }
     val text: LiveData<String> = _text
 }
